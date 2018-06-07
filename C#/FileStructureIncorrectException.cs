8947d02946a96f3dc2f09c7f91f6fa13accc5fc1
mattiabenaglia <39345182+mattiabenaglia@users.noreply.github.com>
2018-06-07 14:45:35 +0200

    Delete FileStructureIncorrectException.cs

--- C#/FileStructureIncorrectException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Elfo.FileImport.Exceptions
-{
-    public class FileStructureIncorrectException : System.Exception
-    {
-        public FileStructureIncorrectException(string message) : base(message){ }
-    }
-}
